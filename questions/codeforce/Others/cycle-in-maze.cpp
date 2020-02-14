6fc9c0a5f6532626456c78bba2e991b60fe82605
unknown <matheuusrdgsf@gmail.com>
2020-02-14 12:26:10 -0300

    cycle-maze update

--- questions/codeforce/Others/cycle-in-maze.cpp
+++ questions/codeforce/Others/cycle-in-maze.cpp
@@ -3,58 +3,129 @@
 #define rep(n) for(int ii = 0; ii < n; ++ ii)
 
 int n, m, k;
-const int ms = 1005;
-//std::vector<char[ms][ms]> maze;
+const int ms = 1015;
 char s[ms][ms];
+
+std::string backtracking(int posX, int posY, int posXinitial, int posYinitial, std::string res, char maze[ms][ms]){
+	//Break Point
+	if (posX == posXinitial and posY == posYinitial){
+		return res;
+	}
+
+	std::vector<std::string> v;
+
+	if (maze[posX][posY+1] == '.' and (posY+1 < m)){ //RIGHT
+		maze[posX][posY+1] = 'X';
+		maze[posX][posY] = '*';
+
+		v.push_back(backtracking( posX , posY+1, posXinitial, posYinitial, res+"R", maze));
+
+		maze[posX][posY+1] = '.';
+		maze[posX][posY] = 'X';
+
+	}
+
+	if(maze[posX-1][posY] == '.' and (posX-1 >= 0) ){  //UP
+		maze[posX-1][posY] = 'X';
+		maze[posX][posY] = '*';
+
+		v.push_back(backtracking( posX-1 , posY, posXinitial, posYinitial, res+"U", maze));
+
+		maze[posX-1][posY] = '.';
+		maze[posX][posY] = 'X';
+
+	}
+
+	if (maze[posX][posY-1] == '.' and (posY-1 >= 0)){ //LEFT
+		maze[posX][posY-1] = 'X';
+		maze[posX][posY] = '*';
+
+		v.push_back(backtracking( posX , posY-1, posXinitial, posYinitial, res+"L", maze));
+
+		maze[posX][posY-1] = '.';
+		maze[posX][posY] = 'X';
+	}
+
+	if (maze[posX+1][posY] == '.' and (posX+1 < n)){ //DOWN
+
+		maze[posX+1][posY] = 'X';
+		maze[posX][posY] = '*';
+
+		v.push_back(backtracking( posX+1 , posY, posXinitial, posYinitial, res+"D", maze));
+
+		maze[posX+1][posY] = '.';
+		maze[posX][posY] = 'X';
+	}
+
+	std::string local = "ZZZZZZZ";
+
+	for (auto i : v){
+		if (lexicographical_compare(local.begin(), local.end(), i.begin(), i.end())){
+			local = i;
+		}
+	}
+
+	if (lexicographical_compare(res.begin(), res.end(), local.begin(), local.end())){
+		return res;
+	}else{
+		return local;
+	}
+
+
+}
+
 
 int solve(int posX, int posY, std::string res, int resLenght){
 
-	std::string resRev;
+	int posXinitial = posX;
+	int posYinitial = posY;
 
-	while (1){	
+	while (1){
+
 		//QTD MOVIMENTOS
 		if (resLenght == k/2){
-			std::cout<<res;
-			reverse(resRev.begin(), resRev.end());
-			std::cout<<resRev;
-			return 1;
+			std::string back = backtracking(posX, posY, posXinitial, posYinitial, "", s);
+			if (back != "ZZZZZZZ"){
+				std::cout<<res+back;
+				return 1;
+			}else{
+				std::cout<<back;
+				std::cout<<"IMPOSSIBLE";
+				return 0;
+			}
 		}
 
-		if (s[posX+1][posY] == '.'){ //DOWN
+		if (s[posX+1][posY] == '.' and (posX+1 < n)){ //DOWN
 
 			s[posX+1][posY] = 'X';
 			s[posX][posY] = '.';
 
 			posX+=1;
 			res+="D";
-			resRev+="U";
 			resLenght+=1;
 
-		}else if (s[posX][posY-1] == '.'){ //LEFT
+		}else if (s[posX][posY-1] == '.' and (posY-1 >= 0)){ //LEFT
 			s[posX][posY-1] = 'X';
 			s[posX][posY] = '.';
 
 			posY-=1;
 			res+="L";
-			resRev+="R";
 			resLenght+=1;
 
-		}else if (s[posX][posY+1] == '.'){ //RIGHT
+		}else if (s[posX][posY+1] == '.' and (posY+1 < m)){ //RIGHT
 			s[posX][posY+1] = 'X';
 			s[posX][posY] = '.';
 
 			posY+=1;
 			res+="R";
-			resRev+="L";
 			resLenght+=1;
 
-		}else if(s[posX-1][posY] == '.'){  //UP
+		}else if(s[posX-1][posY] == '.' and (posX-1 >= 0) ){  //UP
 			s[posX-1][posY] = 'X';
 			s[posX][posY] = '.';
 
 			posX-=1;
 			res+="U";
-			resRev+="D";
 			resLenght+=1;
 		} else {
 			std::cout<<"IMPOSSIBLE";
@@ -74,12 +145,12 @@ int main() {
 	int posY = 0;
 	int flag = 0;
 
-	for(int i = 0;i < n;i++){
+	for(int i = 0; i < n; i++){
 		std::cin>>s[i];
 		
 		if (!flag){
 
-			for(int j = 0;j < m;j++){
+			for(int j = 0; j < m;j++){
 				if (s[i][j] == 'X'){
 		    		posX = i;
 		    		posY = j;
